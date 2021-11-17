42e50cc5baf0a6b8ecbcfd8d7c7001c603e20f95
unknown <fernando.fincatti@pay4fun01.local>
2021-11-17 16:32:23 -0300

    feat: adicionando banco de dados

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,13 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+load_dotenv()
+
+#configuração do banco de dados
+SERVER=os.getenv("SERVER")
+USER=os.getenv("USER")
+PASS=os.getenv("PASS")
+DB=os.getenv("DB")
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{USER}:{PASS}@{SERVER}/{DB}"
+print("HERE -> ", SQLALCHEMY_DATABASE_URL)
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
     connect_args={"check_same_thread": False}
 )
+
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
